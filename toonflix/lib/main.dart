4cf2d141d625de6a7245f66e63c3984d629d0581
JeongWonYoung <nicednjsdud@naver.com>
2023-01-29 23:39:29 +0900

    Made button section

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -11,8 +11,11 @@ class App extends StatelessWidget {
       home: Scaffold(
           backgroundColor: Color(0xFF181818),
           body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40),
+            padding: EdgeInsets.symmetric(
+              horizontal: 40,
+            ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
                   height: 80,
@@ -26,8 +29,8 @@ class App extends StatelessWidget {
                         Text('Hey, BoB',
                             style: TextStyle(
                                 color: Colors.white,
-                                fontSize: 34,
-                                fontWeight: FontWeight.w600)),
+                                fontSize: 28,
+                                fontWeight: FontWeight.w800)),
                         Text('Welcome back',
                             style: TextStyle(
                               color: Colors.white.withOpacity(0.8),
@@ -35,6 +38,52 @@ class App extends StatelessWidget {
                             )),
                       ],
                     )
+                  ],
+                ),
+                SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 44,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 50,
+                        ),
+                        child: Text(
+                          'Transfer',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 )
               ],
