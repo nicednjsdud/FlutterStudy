76049c966cff434ec15502c25fd31d7f5dda4e89
JeongWonYoung <nicednjsdud@naver.com>
2023-01-31 22:34:42 +0900

    Made counter to study stateful widget

--- toonflix2/lib/main.dart
+++ toonflix2/lib/main.dart
@@ -10,13 +10,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  void onClicked() {
+    counter = counter + 1;
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDD8),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
