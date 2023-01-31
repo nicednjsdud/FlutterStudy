d8ad9e272d74e64eb60cd1914e4cca1e9c1ed80d
JeongWonYoung <nicednjsdud@naver.com>
2023-01-31 23:00:46 +0900

    Study build context

--- toonflix2/lib/main.dart
+++ toonflix2/lib/main.dart
@@ -11,38 +11,53 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   // int counter = 0;
-  List<int> numbers = [];
+  // List<int> numbers = [];
 
-  void onClicked() {
-    setState(() {
-      // counter = counter + 1
-      numbers.add(numbers.length);
-    });
-  }
+  // void onClicked() {
+  //   setState(() {
+  //     // counter = counter + 1
+  //     numbers.add(numbers.length);
+  //   });
+  // }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDD8),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(fontSize: 30),
-              ),
-              for (var n in numbers) Text('$n'),
-              IconButton(
-                iconSize: 40,
-                onPressed: onClicked,
-                icon: const Icon(Icons.add_box_rounded),
-              ),
+            children: const [
+              MyLargeTilte(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTilte extends StatelessWidget {
+  const MyLargeTilte({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
