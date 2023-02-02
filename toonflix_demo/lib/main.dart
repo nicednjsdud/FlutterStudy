02538918b51e50fb353642c945f4bc57d6f4f225
JeongWonYoung <nicednjsdud@naver.com>
2023-02-02 23:24:25 +0900

    Made api.service.dart to crawler today webtoon

--- toonflix_demo/lib/main.dart
+++ toonflix_demo/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix_demo/screens/home_screen.dart';
+import 'package:toonflix_demo/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(MyApp());
 }
 
