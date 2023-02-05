1dca6238d1ca5b16ebe0a22cadc3ce8edcdbd816
JeongWonYoung <nicednjsdud@naver.com>
2023-02-05 22:43:01 +0900

    create detail_screen.dart

--- toonflix_demo/lib/widgets/webtoon_widget.dart
+++ toonflix_demo/lib/widgets/webtoon_widget.dart
@@ -1,45 +1,61 @@
 import 'package:flutter/material.dart';
 
+import '../screens/detail_screen.dart';
+
 class Webtoon extends StatelessWidget {
   final String title, thumb, id;
 
   const Webtoon({
+    Key key,
     this.title,
     this.thumb,
     this.id,
-  });
-
-  const Webtoon({Key key}) : super(key: key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 250,
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15,
-                  offset: const Offset(10, 10),
-                  color: Colors.black.withOpacity(0.5),
-                ),
-              ]),
-          child: Image.network(webtoon.thumb),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          webtoon.title,
-          style: const TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(
+              title: title,
+              thumb: thumb,
+              id: id,
+            ),
+            fullscreenDialog: true,
           ),
-        ),
-      ],
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 250,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ]),
+            child: Image.network(thumb),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
